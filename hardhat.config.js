00c12650e78c363443e9fe2250db20e068b5b05b
Raytlty <alexmao930918@gmail.com>
2021-09-02 11:24:45 +0800

    update abi directory

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require('hardhat-abi-exporter');
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
